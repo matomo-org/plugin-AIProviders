9e1e48055003cd63da9697021f17f679148fe13c
Maximilian Taube <max.taube@hotmail.com>
2026-06-29 20:05:38 +0200

    fix translation key issue

--- AIProviders.php
+++ AIProviders.php
@@ -91,6 +91,7 @@ class AIProviders extends Plugin
         $translations[] = 'AIProviders_ApiKey';
         $translations[] = 'AIProviders_ApiKeyAlreadyConfiguredPlaceholder';
         $translations[] = 'AIProviders_ApiKeyPlaceholder';
+        $translations[] = 'AIProviders_BedrockDescription';
         $translations[] = 'AIProviders_ClickTestConnectionToShowAvailableModels';
         $translations[] = 'AIProviders_ConfigurationIntro';
         $translations[] = 'AIProviders_CustomProviderDescription';
